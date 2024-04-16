87915c36e54d404acbd6275f4139b7b4e9213a2f
rs-santiago <rs.santiago1997@gmail.com>
2024-04-16 15:29:13 -0300

    estacionamente

---
+++ script.ts
@@ -0,0 +1,80 @@
+interface IVeiculo {
+    nome: string;
+    placa: string;
+    entrada: Date | string;
+}
+
+(function() {
+    const $ = (query: string): HTMLInputElement | null => 
+        document.querySelector(query)
+
+    function calcTempo(mil: number): string {
+        const min = Math.floor(mil / 60000);
+        const sec = Math.floor((mil % 60000) / 1000);
+
+        return `${min}m e ${sec}s`
+    }
+    function patio() {
+        function ler(): Array<IVeiculo> {
+            return localStorage.patio ? JSON.parse(localStorage.patio) : []
+        }
+
+        function salvar(veiculo: Array<IVeiculo>) {
+            localStorage.setItem("patio", JSON.stringify(veiculo))
+        }
+        
+        function adicionar(veiculo: IVeiculo, salva: boolean) {
+            const row = document.createElement("tr")
+
+            row.innerHTML = `
+                <td>${veiculo.nome}</td>
+                <td>${veiculo.placa}</td>
+                <td>${veiculo.entrada}</td>
+                <td>
+                    <button class="delete" data-placa="${veiculo.placa}">x</button>
+                </td>
+            `;
+
+            row.querySelector(".delete")?.addEventListener("click", function() {
+                remover(this.dataset.placa)
+            })
+            $("#patio")?.appendChild(row)
+            if (salva) salvar([...ler(), veiculo])
+        }
+
+        function remover(placa: string) {
+            const { entrada, nome } = ler().find(veiculo => veiculo.placa === placa)
+
+            const tempo = calcTempo(new Date().getTime() - new Date(entrada).getTime())
+
+            if (!confirm(`o veiculo ${nome} permaneceu por ${tempo}. Deseja encerrar?`)) return;
+
+            salvar(ler().filter((veiculo) => veiculo.placa !== placa));
+            render();
+        }
+
+        function render() {
+            $("#patio")!.innerHTML = "";
+            const patio = ler();
+
+            if (patio.length) {
+                patio.forEach((veiculo) => adicionar(veiculo, false));
+            }
+        }
+
+        return {ler, adicionar, remover, salvar, render}
+    }
+
+    patio().render();
+    $("#cadastrar")?.addEventListener("click", () => {
+        const nome = $("#nome")?.value;
+        const placa = $("#placa")?.value;
+
+        if ( !nome || !placa) {
+            alert("Os campos nomes e placa são obrigatórios")
+            return
+        }
+
+        patio().adicionar({ nome, placa, entrada: new Date().toISOString() }, true)
+    })
+})()
